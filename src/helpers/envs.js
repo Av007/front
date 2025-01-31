135bdf7c20ee9cbe8bb35ae6501e4f07ad2a4f18
Av007 <avdeevvladimir@gmail.com>
2025-01-31 17:28:07 +0200

    debug

--- src/helpers/envs.js
+++ src/helpers/envs.js
@@ -1,7 +1,7 @@
 export const getEnvsUrl = () => {
-    console.log(import.meta.env, window.ENV, process.env, 'env');
+    console.log(import.meta.env, window.ENV, process.env, 'test');
     // eslint-disable-next-line no-undef
-    const URL = window.ENV.VITE_URL || import.meta.env.VITE_URL || process.env.VITE_URL;
+    const URL = import.meta.env.VITE_URL || process.env.VITE_URL;
     console.log(URL, 'url', 'env');
     return URL;
     
