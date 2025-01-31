526b4e93cb1a416b9034fa8ee0d154e425ecc4af
Av007 <avdeevvladimir@gmail.com>
2025-01-31 16:47:08 +0200

    debug

--- src/helpers/envs.js
+++ src/helpers/envs.js
@@ -1,7 +1,8 @@
 export const getEnvsUrl = () => {
+    console.log(import.meta.env);
     // eslint-disable-next-line no-undef
     const URL = import.meta.env.VITE_URL || process.env.VITE_URL;
-    console.log(URL, 'url');
+    console.log(URL, 'url', 'env');
     return URL;
     
 }
