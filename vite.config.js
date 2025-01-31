94831bdc46a5be82d6f5364c613c9e7ad0ba5cf1
Av007 <avdeevvladimir@gmail.com>
2025-01-31 16:51:00 +0200

    rollback vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env,
-    },
-    server: {
-      port: 5173,
-      open: true,
-    },
-    build: {
-      outDir: 'dist',
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
